27b4bee45e15877913591ab2bc5c8245100eb119
Blueberryy <igory.ygr200@gmail.com>
2020-09-05 22:20:49 +0500

    Delete cl_language.lua

--- lua/easydoorbell/cl_language.lua
+++
@@ -1,9 +0,0 @@
---------------------------------------------------------------------------------
--- Easy Doorbell Language File
--- Edit this file to add your custom translations
---------------------------------------------------------------------------------
-
-if CLIENT then
-end
-
---We're moved to resource\localization
